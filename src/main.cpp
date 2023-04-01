ce330653c12e4f31f0d29191d196753084c382cc
Rich González <ragparkour@gmail.com>
2023-04-01 16:14:32 -0600

    ajuste de variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
 #include <Arduino.h>
 
-uint8_t _sensorTinaco0    = 23;
-uint8_t _sensorTinaco100  = 13;
-uint8_t _releDeBomba      = 2;
-uint8_t botonInterruptor  = 12;
+const uint8_t _sensorTinaco0    = 23;
+const uint8_t _sensorTinaco100  = 13;
+const uint8_t _releDeBomba      = 2;
+const uint8_t botonInterruptor  = 12;
 
-uint64_t tiempoDeFalso = 800;
+uint32_t tiempoDeFalso = 800;
 bool estadoBomba       = false; //False = OFF | True = ON
 
 //TIMERS
 uint64_t tiempoAnterior               = 0;
-
-uint64_t tiempoBomba                 = 3000000; // 50min  = 3000000 mseg
+uint64_t tiempoBomba                 = 3000000; // 50min  = 3 000000 mseg
 uint64_t tiempoBomba_previo          = 0;
 uint64_t tiempoBombaMargen           = 300000;  // 5min   = 300000  mseg
 uint64_t tiempoBombaMargen_previo    = 0;
 uint64_t tiempoBombaReceso           = 7200000; // 2hrs   = 7200000 mseg
 uint64_t tiempoBombaReceso_previo    = 0;
 
-uint64_t tiempolecturaSensores        = 1000; // 10seg = 10,000 mseg
+uint64_t tiempolecturaSensores        = 10000; // 10seg = 10,000 mseg
 uint64_t tiempolecturaSensores_previo = 0;
 
 // Falta considerar el caso donde no haya agua en la toma general y la bomba se quede encendida sin pasar agua
@@ -35,7 +34,6 @@ void encenderBomba(){
   digitalWrite(_releDeBomba,HIGH);
   tiempoBomba_previo = millis();
   tiempoBombaMargen_previo = millis();
-
 }
 
 void controlTinaco() {
@@ -43,6 +41,7 @@ void controlTinaco() {
   bool sensorTinaco0    = digitalRead(_sensorTinaco0);
   // bool sensorTinaco100  = digitalRead(_sensorTinaco100);
 
+  // Si la bomba está encendida
   if (estadoBomba) {
 
     // Si pasa el tiempoBomba, la bomba se apaga
@@ -58,6 +57,7 @@ void controlTinaco() {
 
   //Si la bomba está apagada
   } else {
+    // Si sensor de tinaco está vacío Y el delta de tiempo es mayor que el tiempoBombaReceso
     if (!sensorTinaco0 && millis() - tiempoBombaReceso_previo > tiempoBombaReceso) {
       Serial.println("Tinaco_N_0 Bomba ON");
       encenderBomba();
@@ -69,9 +69,9 @@ void controlTinaco() {
 void setup() {
   Serial.begin(115200);
 
-  pinMode(_sensorTinaco0, INPUT_PULLDOWN);
-  pinMode(_sensorTinaco100,  INPUT);
-  pinMode(_releDeBomba,       OUTPUT);
+  pinMode(_sensorTinaco0,   INPUT_PULLDOWN);
+  pinMode(_sensorTinaco100, INPUT_PULLDOWN);
+  pinMode(_releDeBomba,     OUTPUT);
 
   //Instalar una resistencia Pull-down
   // attachInterrupt(digitalPinToInterrupt(botonInterruptor), cambiarEstadoBomba, FALLING);
@@ -79,6 +79,7 @@ void setup() {
 }
 
 void loop() {
+
   if (millis() - tiempolecturaSensores_previo > tiempolecturaSensores) {
     Serial.print("SensorT0: ");
     Serial.print(digitalRead(_sensorTinaco0));
@@ -87,13 +88,10 @@ void loop() {
     Serial.print(" TiempoReceso: ");
     Serial.println(millis() - tiempoBombaReceso_previo);
 
-
-
     controlTinaco();
 
     tiempolecturaSensores_previo = millis();
   }
-  
-  // put your main code here, to run repeatedly:
+
 }
 
